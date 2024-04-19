d386246c253503700fa81503502f5c7e26899aac
Jonas Bunokas <135608512+jonasbunokas@users.noreply.github.com>
2024-04-19 13:51:14 +0300

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+//var generateName = require("sillyname");
+
+//import generateName from "sillyname";
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+//var sillyName = generateName();
+
+//console.log(`My name is ${sillyName}.`);
+
+console.log(`My name is ${name}!`);
